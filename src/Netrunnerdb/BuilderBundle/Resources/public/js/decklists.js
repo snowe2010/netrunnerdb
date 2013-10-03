9fabd0e437b88997d1be864f3429df539a5454a9
bertolini.cedric@gmail.com <bertolini.cedric@gmail.com>
2013-10-03 22:02:56 +0200

    Tooltip display in Cards list mode. Transparency in list mode.

--- src/Netrunnerdb/BuilderBundle/Resources/public/js/decklists.js
+++ src/Netrunnerdb/BuilderBundle/Resources/public/js/decklists.js
@@ -1,22 +1,19 @@
+var CardDB = null,
+	SetDB = null;
 
 function when_all_parsed() {
-console.log('when_all_parsed: IsModified '+IsModified);
-if(CardDB && IsModified === false) return;
-var sets_data = SetsData || JSON.parse(localStorage.getItem('sets_data_'+Locale));
-if(!sets_data) {
-	console.log('no data');
-	return;
-}
-SetDB = TAFFY(sets_data);
-SetDB({name:"Promos"}).remove();
+	if(CardDB && IsModified === false) return;
+	var sets_data = SetsData || JSON.parse(localStorage.getItem('sets_data_'+Locale));
+	if(!sets_data) return;
+	SetDB = TAFFY(sets_data);
+	SetDB({code:"promos"}).remove();
 
-var cards_data = CardsData || JSON.parse(localStorage.getItem('cards_data_'+Locale));
-CardDB = TAFFY(cards_data);
-CardDB({setname:"Promos"}).remove();
-console.log('when_all_parsed: '+CardDB().count()+' cards in database');
+	var cards_data = CardsData || JSON.parse(localStorage.getItem('cards_data_'+Locale));
+	CardDB = TAFFY(cards_data);
+	CardDB({setcode:"promos"}).remove();
 }
 
 $(function() {
-when_all_parsed();
-$.when(promise1, promise2).done(when_all_parsed);
+	when_all_parsed();
+	$.when(promise1, promise2).done(when_all_parsed);
 });
