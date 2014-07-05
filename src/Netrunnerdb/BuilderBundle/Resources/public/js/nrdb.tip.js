00a604b1b319b09ee5ba221e0b2a362c17dc9cc2
Alsciende <bertolini.cedric@gmail.com>
2014-07-05 11:38:34 +0200

    inline svg only

--- src/Netrunnerdb/BuilderBundle/Resources/public/js/nrdb.tip.js
+++ src/Netrunnerdb/BuilderBundle/Resources/public/js/nrdb.tip.js
@@ -24,7 +24,9 @@ NRDB.tip = {};
 		$(this).qtip(
 				{
 					content : {
-						text : '<div class="card-image card-image-'+card.side_code+'-'+card.type_code+'"'+(card.imagesrc ? ' style="background-image:url('+card.imagesrc+')"': '')+'><img src="/web/bundles/netrunnerdbbuilder/images/hexagon.svg"></div><h4>' + (card.uniqueness ? "&diams; " : "")
+						text : '<div class="card-image card-image-'+card.side_code+'-'+card.type_code+'"'+(card.imagesrc ? ' style="background-image:url('+card.imagesrc+')"': '')
+								+ '><svg width="103px" height="90px" viewBox="0 0 677 601" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><rect x="0" y="0" width="677" height="601" fill="white" fill-opacity="1" mask="url(#mask)"/><use xlink:href="#hex" style="stroke:black; fill:none; stroke-width:15" /></svg></div><h4>'
+								+ (card.uniqueness ? "&diams; " : "")
 								+ card.title + '</h4>' + type
 								+ '<div class="card-text">' + NRDB.format.text(card) + '</div>'
 								+ '<p class="card-faction" style="text-align:right">' + influence
@@ -42,7 +44,8 @@ NRDB.tip = {};
 						event : event.type,
 						ready : true,
 						solo : true
-					}
+					},
+					hide: { event: 'unfocus' }
 				}, event);
 	};
 
