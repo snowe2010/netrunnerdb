537013eec189f7d015eb99ae34a2d307607dc2a7
bertolini.cedric@gmail.com <bertolini.cedric@gmail.com>
2014-07-14 17:53:57 +0200

    add fuzzy search module ; use it in directimport ; fix #120

--- src/Netrunnerdb/BuilderBundle/Resources/public/js/directimport.js
+++ src/Netrunnerdb/BuilderBundle/Resources/public/js/directimport.js
@@ -1,9 +1,4 @@
 NRDB.data_loaded.add(function() {
-	NRDB.data.cards({set_code:"alt"}).remove();
-	CardNames = [];
-	NRDB.data.cards().each(function (record, recordnumber) {
-		CardNames.push({code: record.code, title: record.title, type: record.type, token: record.title.replace(/\W+/, ' ').trim().toLowerCase()});
-	});
 	$('#btn-import').prop('disabled', false);
 });
 
@@ -33,36 +28,8 @@ function do_import() {
 	update_stats();
 }
 function import_one_line(line, lineNumber) {
-	var qty, name = line.replace(/\(.*\)/, '').replace(/\W+/g, ' ').replace(/\s+/, ' ').trim().toLowerCase();
-	if(name.match(/^(\d+)x?\s*(.*)/)) {
-		qty = parseInt(RegExp.$1, 10);
-		name = RegExp.$2;
-	} else if(name.match(/(.*)\s*x?(\d+)$/)) {
-		qty = parseInt(RegExp.$2, 10);
-		name = RegExp.$1;
-	}
-	if(name == "") return;
-	var options = [];
-	var query = NRDB.data.cards({token: {likenocase:name}});
-	if(query.count() == 1) {
-		var record = query.first();
-		options.push({code: record.code, name: record.title, type: record.type});
-	} else if(query.count() > 1) {
-		query.each(function (record,recordnumber) {
-			options.push({code: record.code, name: record.title, type: record.type});
-		});
-	} else if(query.count() == 0) {
-		var matches = [];
-		$.each(CardNames, function(index, row) {
-			var score = row.token.score(name, 0.9);
-			matches.push({code: row.code, title: row.title, type: row.type, score: score});
-		});
-		matches.sort(function (a,b) { return a.score > b.score ? -1 : a.score < b.score ? 1 : 0 });
-		var bestScore = matches[0].score;
-		for(var i=0; i<5 && matches[i].score > 0.4 && matches[i].score > bestScore * 0.9; i++) {
-			options.push({code: matches[i].code, name: matches[i].title, type: matches[i].type});
-		}
-	}
+    var result = NRDB.fuzzy_search.lookup(line);
+	var options = result.cards, qty = result.qty;
 	var qty_text = "", qty_int = qty;
 	if(qty == null) {
 		options = $.grep(options, function (element) {
@@ -78,13 +45,13 @@ function import_one_line(line, lineNumber) {
 		return '<i>No match for '+name+'</i>';
 	} else if(options.length == 1) {
 		return '<input type="hidden" name="'+lineNumber+'" value="'+options[0].code+':'+qty_int+'">'
-		+qty_text+'<a class="card" data-code="'+options[0].code+'" href="#">'+options[0].name+' </a>';
+		+qty_text+'<a class="card" data-code="'+options[0].code+'" href="#">'+options[0].title+' </a>';
 	} else {
 		var text = '<input type="hidden" name="'+lineNumber+'" value="'+options[0].code+':'+qty_int+'">'
-		+qty_text+'<a class="card dropdown-toggle text-warning" data-toggle="dropdown" data-code="'+options[0].code+'" href="#">'+options[0].name+' <span class="caret"></span></a>';
+		+qty_text+'<a class="card dropdown-toggle text-warning" data-toggle="dropdown" data-code="'+options[0].code+'" href="#">'+options[0].title+' <span class="caret"></span></a>';
 		text += '<ul class="dropdown-menu">';
 		$.each(options, function (index, option) {
-			text += '<li><a href="#" data-code="'+option.code+'">'+option.name+'</a></li>';
+			text += '<li><a href="#" data-code="'+option.code+'">'+option.title+'</a></li>';
 		});
 		text += '</ul>';
 		return text;
