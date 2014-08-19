929fde534af0db2ebc55eec99466724775ef8608
Alsciende <bertolini.cedric@gmail.com>
2014-08-19 15:50:33 +0200

    remove card image in tooltip for Firefox. fix #129

--- src/Netrunnerdb/BuilderBundle/Resources/public/js/nrdb.tip.js
+++ src/Netrunnerdb/BuilderBundle/Resources/public/js/nrdb.tip.js
@@ -22,7 +22,8 @@ NRDB.tip = {};
 		if (card.strength != null)
 			type += '<p>Strength <b>' + card.strength + '</b></p>';
 		var image_svg = ''; 
-		if($('#nrdb_svg_hex').length) {
+		if($('#nrdb_svg_hex').length && typeof InstallTrigger === 'undefined') {
+			// no hexagon for Firefox, bad boy!
 			image_svg = '<div class="card-image card-image-'+card.side_code+'-'+card.type_code+'"'+(card.imagesrc ? ' style="background-image:url('+card.imagesrc+')"': '')
 			+ '><svg width="103px" height="90px" viewBox="0 0 677 601" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><mask id="mask"><use xlink:href="#rect" style="fill:white" /><use xlink:href="#hex" style="fill:black"/></mask><use xlink:href="#rect" mask="url(#mask)"/><use xlink:href="#hex" style="stroke:black;fill:none;stroke-width:15" /></svg></div>';
 		}
@@ -49,7 +50,10 @@ NRDB.tip = {};
 						event : event.type,
 						ready : true,
 						solo : true
-					}
+					}/*,
+					hide : {
+						event: 'unfocus'
+					}*/
 				}, event);
 	};
 
